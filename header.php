9726a00152fb58191926a64b89d4f5fe1a93056b
xp3rt4g <tom.povse@gmail.com>
2020-09-09 09:54:08 +0200

    added header

---
+++ header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avto.met</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid GO-headerwrapper p-0">
+    <div class="row m-0">
+    	<div class="container GO-headermid">
+              <div class="row">
+                  <!--LOGO -->
+                  <div class="col-4 pl-0">
+                    <a target="_self" href="https://www.avto.net">
+                    <img name="Avto.net" src="https://www.avto.net/_graphics/avtonet/logo11.png" alt="www.avto.net" class="GO-logo"/>                            </a>
+                   </div>
+                    <!--/  LOGO -->
+                    <!-- NAV -->
+                    <div class="col-8 d-md-none GO-moj-menu p-0 m-0 text-right">
+                        
+                     <!-- Hamburger-->
+                      <button class="navbar-toggler first-button border-0" type="button" data-toggle="offcanvas">
+                          <div class="animated-icon1">
+                              <span></span><span></span><span></span>
+                            </div>
+                      </button>
+                    </div>
+                    <div class="d-none d-md-block col-8 GO-moj-menu p-0 m-0">
+                    <ul>
+                      <li>
+                         <a target="_blank" href="https://www.avto.net/_2016mojavtonet/ad_select_rubric_icons.asp">
+                          <span class="GO-moj-menu-hover mx-1 px-1">
+                          <i class="fa fa-plus-circle mx-1"></i>
+                          Objavi oglas
+                          </span>
+                          </a>
+                      </li>
+                      <li>
+                         <a href="https://www.avto.net/_2016mojavtonet/parkirano.asp" target="_blank">
+                          <span class="GO-moj-menu-hover mx-1 px-1">
+                          <i class="fa fa-star mx-1"></i>
+                          Parkirano
+                          </span>
+                        </a>
+                      </li>
+                      <li>
+                         <a href="https://moj.avto.net" target="_blank">
+                          <span class="GO-moj-menu-dark mx-1 px-2">
+                          <i class="fa fa-user mx-1"></i>
+                          moj.avto.net
+                          </span>
+                          </a>
+                      </li>
+                    </ul>                
+                    </div>
+                    <!--/  NAV-->
+                </div><!--/  ROW -->
+		</div><!--/  CONTAINER -->
+	</div><!--/  ROW -->
+    
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+include_once './header.php';
+
+?>
+
+<body class="h1">
+
+TEST
+    
+</body>
+
+<?php 
+
+include_once './footer.php';
+
+?>
