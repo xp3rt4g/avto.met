bf2b9a11d2c4b6efb4ee754eab545eb7396ab3bb
xp3rt4g <tom.povse@gmail.com>
2020-09-16 08:37:57 +0200

    added header

--- header.php
+++ header.php
@@ -5,20 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Avto.met</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-<div class="container-fluid GO-headerwrapper p-0">
+<!-- SHOW HEADER -->
+<div class="container-fluid headerwrapper p-0">
     <div class="row m-0">
-    	<div class="container GO-headermid">
+    	<div class="container headermid">
               <div class="row">
                   <!--LOGO -->
                   <div class="col-4 pl-0">
                     <a target="_self" href="https://www.avto.net">
-                    <img name="Avto.net" src="https://www.avto.net/_graphics/avtonet/logo11.png" alt="www.avto.net" class="GO-logo"/>                            </a>
+                    <img name="Avto.net" src="img/logo.png" alt="www.avto.met" class="logo"/>                            </a>
                    </div>
                     <!--/  LOGO -->
                     <!-- NAV -->
-                    <div class="col-8 d-md-none GO-moj-menu p-0 m-0 text-right">
+                    <div class="col-8 d-md-none moj-menu p-0 m-0 text-right">
                         
                      <!-- Hamburger-->
                       <button class="navbar-toggler first-button border-0" type="button" data-toggle="offcanvas">
@@ -27,11 +30,11 @@
                             </div>
                       </button>
                     </div>
-                    <div class="d-none d-md-block col-8 GO-moj-menu p-0 m-0">
+                    <div class="d-none d-md-block col-8 moj-menu p-0 m-0">
                     <ul>
                       <li>
                          <a target="_blank" href="https://www.avto.net/_2016mojavtonet/ad_select_rubric_icons.asp">
-                          <span class="GO-moj-menu-hover mx-1 px-1">
+                          <span class="moj-menu-hover mx-1 px-1">
                           <i class="fa fa-plus-circle mx-1"></i>
                           Objavi oglas
                           </span>
@@ -39,7 +42,7 @@
                       </li>
                       <li>
                          <a href="https://www.avto.net/_2016mojavtonet/parkirano.asp" target="_blank">
-                          <span class="GO-moj-menu-hover mx-1 px-1">
+                          <span class="moj-menu-hover mx-1 px-1">
                           <i class="fa fa-star mx-1"></i>
                           Parkirano
                           </span>
@@ -47,7 +50,7 @@
                       </li>
                       <li>
                          <a href="https://moj.avto.net" target="_blank">
-                          <span class="GO-moj-menu-dark mx-1 px-2">
+                          <span class="moj-menu-dark mx-1 px-2">
                           <i class="fa fa-user mx-1"></i>
                           moj.avto.net
                           </span>
@@ -59,6 +62,33 @@
                 </div><!--/  ROW -->
 		</div><!--/  CONTAINER -->
 	</div><!--/  ROW -->
+	<!-- / SUBMENU - RUBRIKE-->
+    <div class="d-none d-lg-block ddcolortabsFULLWIDTH">
+        <div id="droplinemenu" class="ddcolortabs">
+            <ul class="p-0 m-0">
+            <li><a href="https://www.avto.net" rel="submenuHOME" class="first"><span class="first"><i class="fa fa-home fa-lg"></i></span></a></li>
+            <li><a href="https://www.avto.net/Ads/search_category.asp?SID=10000" rel="submenuAVTO"><span>Avto</span></a></li>
+            </ul>
+        </div>
+       </div>
+	<!-- / DDCOLORTABS-->
+    <!-- OLD SUB TABS -->
+    <div class="tabcontainerFULLWIDTH d-none">
+            <div class="tabcontainer">
+                <div id="submenuHOME" class="tabcontent"></div>
+                <div id="submenuAVTO" class="tabcontent"></div>
+                <div id="submenuMOTO" class="tabcontent"></div>
+                <div id="submenuGOSPODARSKA" class="tabcontent"></div>
+                <div id="submenuMEHANIZACIJA" class="tabcontent"></div>
+                <div id="submenuPROSTICAS" class="tabcontent"></div>
+                <div id="submenuOPREMA" class="tabcontent"></div>
+                <div id="submenuMOJAVTONET" class="tabcontent"></div>
+                <div id="submenuRUBRIKE" class="tabcontent"></div>
+            </div>
+    </div>   
+	<!-- / OLD SUB TABS-->    
+</div>
+<!--/  SHOW HEADER -->
     
 </div>
 </body>
