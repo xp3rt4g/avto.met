2cbe1a69f28de45be04ff69c6ab23cfeafed9136
xp3rt4g <tom.povse@gmail.com>
2020-09-30 10:39:27 +0200

    fixed simple and detailed search
    zaščvicu ful

--- search.php
+++ search.php
@@ -7,15 +7,15 @@
 <link rel="stylesheet" href="css/search.css">
 
 <body>
-<div class="container p-0">
-<div class="container bg-white rounded-bottom shadow-box m-0 mb-3">
-        <div class="row pt-3 pb-2 px-3">
-            <div class="col-12 px-0">
-                <h3 class="blue"><strong>Rezultati iskanja</strong></h3>
+    <div class="container p-0">
+        <div class="container bg-white rounded-bottom shadow-box m-0 mb-3">
+            <div class="row pt-3 pb-2 px-3">
+                <div class="col-12 px-0">
+                    <h3 class="blue"><strong>Rezultati iskanja</strong></h3>
+                </div>
             </div>
         </div>
-    </div>
-<?php 
+        <?php 
 
     include 'connect.php';
 
@@ -57,7 +57,7 @@
                 $query .= "ORDER BY c.date_posted;";
 
             }
-            }
+        }
         elseif($search_type == "advanced"){
                 if(isset($_GET['manufacturer']) && isset($_GET['model']) && isset($_GET['priceFrom']) && isset($_GET['priceTo']) && isset($_GET['yearMin']) && isset($_GET['yearMax']) && isset($_GET['kmMin']) && isset($_GET['kmMax']) && isset($_GET['gearbox']) && isset($_GET['fuelType'])  && isset($_GET['ccmMin'])  && isset($_GET['ccmMax'])  && isset($_GET['kwMin'])  && isset($_GET['kwMax']) && isset($_GET['adAge'])  && isset($_GET['sellerType']) && isset($_GET['sellerLocation'])){
     
@@ -264,14 +264,15 @@
                 else{
                     header("Location: car_search.php");
             }
+        }
             ?>
-            
-
-            <div class="col-12">
 
 
+        <div class="col-12">
 
-            
+
+
+
 
             <?php
 
@@ -287,101 +288,104 @@
             
 
             while ($row = $stmt->fetch()) {
-                ?> 
-                
-                <div class="row bg-white mb-3 pb-3 pb-sm-0 position-relative results-row center shadow-dark">
+                ?>
 
-                    <a href="ads.php?id=<?php echo $row['id'] ?>" class="stretched-link"></a>
+            <div class="row bg-white mb-3 pb-3 pb-sm-0 position-relative results-row center shadow-dark">
 
-                    <div class ="bg-dark px-3 py-2 font-weight-bold text-truncate text-white text-decoration-none results-title"><?php echo $row['type'] ?></div>
+                <a href="ads.php?id=<?php echo $row['id'] ?>" class="stretched-link"></a>
 
-                    <div class="col-auto px-3 py-0 py-sm-3 pt-3 photo d-flex justify-content-center align-items-center">
-                    
-                        <div class="photo-display align-self-center">
-                        
-                            <img src="<?php echo $row['url'] ?>" alt="<?php echo $row['type']; ?>" class="img-fluid">
+                <div
+                    class="bg-dark px-3 py-2 font-weight-bold text-truncate text-white text-decoration-none results-title">
+                    <?php echo $row['type'] ?></div>
 
-                        </div>
+                <div class="col-auto px-3 py-0 py-sm-3 pt-3 photo d-flex justify-content-center align-items-center">
 
-                    </div>
+                    <div class="photo-display align-self-center">
 
-                    <div class="col-auto py-0 results-top-data pb-lg-3 mt-0 mt-sm-3 mr-0 pr-0">
-                    
-                        <div class="p-0 m-0 pl-3 pr-3">
-                        
-                            <div class="results-top-data-display">
-                            
-                                <table class="table table-striped table-sm table-borderless font-weight-normal  my-3 my-sm-0">
-                                
-                                    <tbody>
-                                    
-                                        <tr>
-                                        
-                                            <td class="w-25 d-none d-md-block pl-3">1.registracija</td>
-                                            <td class="w-75 pl-3"><?php echo date("Y", strtotime($row['first_registration'])) ?></td>
-                                        
-                                        </tr>
-                                        <tr>
-                                        
-                                            <td class="d-none d-md-block pl-3">Prevoženih</td>
-                                            <td class="pl-3"><?php echo $row['mileage'] ?> km</td>
-                                        
-                                        </tr>
-                                        <tr>
-                                        
-                                            <td class="d-none d-md-block pl-3">Gorivo</td>
-                                            <td class="pl-3"><?php echo $row['fueltype'] ?></td>
-                                        
-                                        </tr>
-                                        <tr>
-                                        
-                                            <td class="d-none d-md-block pl-3">Menjalnik</td>
-                                            <td class="pl-3"><?php echo $row['gearbox'] ?></td>
-                                        
-                                        </tr>
-                                        <tr class="d-none d-md-table-row">
-                                        
-                                            <td class="d-none d-md-block pl-3">Motor</td>
-                                            <td class="pl-3"><?php echo $row['ccm']."ccm, ".$row['power']."kW / ".round(($row['power'] * 1.341), 0)." KM" ?></td>
-                                        
-                                        </tr>
-                                    
-                                    </tbody>
-                                
-                                </table>
-                            
-                            </div>
-                        
-                        </div>
+                        <img src="<?php echo $row['url'] ?>" alt="<?php echo $row['type']; ?>" class="img-fluid">
 
-                        <div class="mt-5 mt-sm-0 pt-0 pt-sm-5 results-price-logo">
-                    
-
-                            <div class="results-price ml-4 ml-sm-0 ml-md-4">
-
-                                <div class="price-show">
-                                
-                                    <div class="price-text">
-                                    
-                                    <?php echo number_format($row['price'] , 0, ',', '.') . " €"; ?>
-
-                                    </div>
-                                
-                                </div>
-                            
-                            </div>
-                        
-                        </div>
-                    
                     </div>
 
                 </div>
-                
-                <?php
+
+                <div class="col-auto py-0 results-top-data pb-lg-3 mt-0 mt-sm-3 mr-0 pr-0">
+
+                    <div class="p-0 m-0 pl-3 pr-3">
+
+                        <div class="results-top-data-display">
+
+                            <table
+                                class="table table-striped table-sm table-borderless font-weight-normal  my-3 my-sm-0">
+
+                                <tbody>
+
+                                    <tr>
+
+                                        <td class="w-25 d-none d-md-block pl-3">1.registracija</td>
+                                        <td class="w-75 pl-3">
+                                            <?php echo date("Y", strtotime($row['first_registration'])) ?></td>
+
+                                    </tr>
+                                    <tr>
+
+                                        <td class="d-none d-md-block pl-3">Prevoženih</td>
+                                        <td class="pl-3"><?php echo $row['mileage'] ?> km</td>
+
+                                    </tr>
+                                    <tr>
+
+                                        <td class="d-none d-md-block pl-3">Gorivo</td>
+                                        <td class="pl-3"><?php echo $row['fueltype'] ?></td>
+
+                                    </tr>
+                                    <tr>
+
+                                        <td class="d-none d-md-block pl-3">Menjalnik</td>
+                                        <td class="pl-3"><?php echo $row['gearbox'] ?></td>
+
+                                    </tr>
+                                    <tr class="d-none d-md-table-row">
+
+                                        <td class="d-none d-md-block pl-3">Motor</td>
+                                        <td class="pl-3">
+                                            <?php echo $row['ccm']."ccm, ".$row['power']."kW / ".round(($row['power'] * 1.341), 0)." KM" ?>
+                                        </td>
+
+                                    </tr>
+
+                                </tbody>
+
+                            </table>
+
+                        </div>
+
+                    </div>
+
+                    <div class="mt-5 mt-sm-0 pt-0 pt-sm-5 results-price-logo">
+
+
+                        <div class="results-price ml-4 ml-sm-0 ml-md-4">
+
+                            <div class="price-show">
+
+                                <div class="price-text">
+
+                                    <?php echo number_format($row['price'] , 0, ',', '.') . " €"; ?>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <?php
             }
-        }
-            
-                        
         }
     }
     else{
@@ -390,7 +394,8 @@
 
 ?>
 
-</div>
-</div>
+        </div>
+    </div>
 </body>
+
 </html>
